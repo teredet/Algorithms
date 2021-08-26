d598159ff2f79a08c41ddc65209d42018ac35776
teredet <artem.vanzytler@gmail.com>
2021-08-26 23:07:58 +0300

    Change SubstitutionСipher. Added the ability to use functions from outside. Added docstrings for functions.

--- Encryption algorithms/SubstitutionСipher.py
+++ Encryption algorithms/SubstitutionСipher.py
@@ -1,8 +1,15 @@
-def crypt(input_string, keys):
+def SubstitutionСipher(message, keys):
+    '''
+    message - the string to encode or decore\n
+    keys - Dictionary with character ratios. When encoding: sign - key. When decoded: key - sign.\n
+    \n
+    The function encodes or decodes the message.\n
+    Returns a string.\n    
+    '''
 
     modified_string = ""
 
-    for i in input_string:
+    for i in message:
         if i in keys:
             modified_string += keys[i]
         else:
@@ -11,41 +18,24 @@ def crypt(input_string, keys):
     return modified_string
 
 
-cryptMode = input("[E]ncrypt|[D]ecrypt: ").upper()
-if cryptMode not in ['E','D']:
-	print("Error: mode is not Found!"); raise SystemExit
+if __name__ == "__main__":
+    while True:
+        print('Enter E to encrypt the message, D to decrypt and EXIT to exit')
+        cryptMode = input('>>> ').upper()
 
-input_string = input('>>> ').upper()
+        if cryptMode == 'EXIT':
+            break
+        elif cryptMode in ['E','D']:
+            message = input('Enter the string: ').upper()
 
-symbolsAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-symbolsCrypt = ['!','@','#','$','%','^','&','*','(',')','-','=', '+','?',':',';','<','>','/','[',']','{','}','|','.',',','~']
+            symbolsAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+            symbolsCrypt = ['!','@','#','$','%','^','&','*','(',')','-','=', '+','?',':',';','<','>','/','[',']','{','}','|','.',',','~']
 
-if cryptMode == 'E':
-    keys = dict(zip(symbolsAlpha,symbolsCrypt))
-    print(f"Encrypted text: {crypt(input_string, keys)}")
-elif cryptMode == 'D':
-    keys = dict(zip(symbolsCrypt, symbolsAlpha))
-    print(f"Decrypted text: {crypt(input_string, keys)}")
-
-
-
-
-#symbolsAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-#symbolsCrypt = ['!','@','#','$','%','^','&','*','(',')','-','=', '+','?',':',';','<','>','/','[',']','{','}','|','.',',','~']
-
-#cryptMode = input("[E]ncrypt|[D]ecrypt: ").upper()
-#if cryptMode not in ['E','D']:
-#	print("Error: mode is not Found!"); raise SystemExit
-#startMessage = input("Write the message: ").upper()
-#keys = dict(zip(symbolsAlpha,symbolsCrypt))
-
-#def encryptDecrypt(mode, message, final = ""):
-#	if mode == 'E':
-#		for symbol in message:
-#			if symbol in keys: final += keys[symbol]
-#	else:
-#		for symbol in message:
-#			for key in keys:
-#				if symbol == keys[key]: final += key
-#	return final
-#print("Final message:",encryptDecrypt(cryptMode, startMessage))
+            if cryptMode == 'E':
+                keys = dict(zip(symbolsAlpha,symbolsCrypt))
+                print(f"Encrypted text: {SubstitutionСipher(message, keys)}")
+            elif cryptMode == 'D':
+                keys = dict(zip(symbolsCrypt, symbolsAlpha))
+                print(f"Decrypted text: {SubstitutionСipher(message, keys)}")
+        else:
+            continue
